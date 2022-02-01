7ad03252d65d687e1c7e656f0b7a1ca9462a29b5
Priyam Srivastava <ipriyam26@gmail.com>
2022-02-01 16:59:49 +0530

    fixed

--- streaming.py
+++ streaming.py
@@ -6,27 +6,27 @@ import os
 from os import path
 
 
-# if(platform.system()=='Windows' and not path.exists("req.txt")):
-#     try:
-#         os.system('iwr -useb get.scoop.sh | iex')
-#     except:
-#         os.system('Set-ExecutionPolicy RemoteSigned -scope CurrentUser')
-#         os.system('iwr -useb get.scoop.sh | iex')   
-#     os.system('scoop install curl')
-#     os.system('scoop install aria2') 
-#     os.system('scoop install nodejs')
-#     os.system('scoop install vlc')
-#     os.system('npm i webtorrent-cli')
-#     with open('req.txt', 'w') as f:
-#         f.write('met')
-# if(platform.system()=='Darwin' and not path.exists("req.txt")) :
-#     with open('req.txt', 'w') as f:
-#         f.write('met') 
-#     os.system('cd ~')
-#     os.system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-#     os.system('brew install iina')
-#     os.system('brew install npm')
-#     os.system('npm i webtorrent-cli')
+if(platform.system()=='Windows' and not path.exists("req.txt")):
+    try:
+        os.system('iwr -useb get.scoop.sh | iex')
+    except:
+        os.system('Set-ExecutionPolicy RemoteSigned -scope CurrentUser')
+        os.system('iwr -useb get.scoop.sh | iex')   
+    os.system('scoop install curl')
+    os.system('scoop install aria2') 
+    os.system('scoop install nodejs')
+    os.system('scoop install vlc')
+    os.system('npm i webtorrent-cli')
+    with open('req.txt', 'w') as f:
+        f.write('met')
+if(platform.system()=='Darwin' and not path.exists("req.txt")) :
+    with open('req.txt', 'w') as f:
+        f.write('met') 
+    os.system('cd ~')
+    os.system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+    os.system('brew install iina')
+    os.system('brew install npm')
+    os.system('npm i webtorrent-cli -g')
     
     
     
@@ -34,7 +34,10 @@ from os import path
 
 Movie = input("What would you like to watch today? ")
 paylode = {'q':Movie}
-r = requests.get('https://apibay.org/q.php',params=paylode)
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
+}
+r = requests.get('https://apibay.org/q.php',params=paylode,headers=headers)
 status = r.status_code
 t =   r.json() if status==200 else None
 if(t!=None):
